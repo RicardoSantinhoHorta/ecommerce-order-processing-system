317af684e2116e1aeed989b93c46c37f74a16863
Ricardosanhorta@gmail.com <Ricardosanhorta@gmail.com>
2026-08-20 17:21:35 +0100

    ADD event producer

---
+++ order-service/src/main/java/order/messaging/OrderEventProducer.java
@@ -0,0 +1,19 @@
+package order.messaging;
+
+import order.event.OrderCreatedEvent;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderEventProducer {
+
+    private final KafkaTemplate<String, OrderCreatedEvent> kafkaTemplate;
+
+    public OrderEventProducer(KafkaTemplate<String, OrderCreatedEvent> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void publishOrderCreated(OrderCreatedEvent event){
+        kafkaTemplate.send("order-created", event);
+    }
+}
