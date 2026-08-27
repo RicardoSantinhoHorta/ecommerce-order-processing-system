be00f2b3683d132b0aaecf5a568fc3742aad3b81
Ricardosanhorta@gmail.com <Ricardosanhorta@gmail.com>
2026-08-27 22:43:56 +0100

    ADD Payment object

--- payment-service/src/main/java/payment/model/Payment.java
+++ payment-service/src/main/java/payment/model/Payment.java
@@ -1,4 +1,36 @@
 package payment.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import payment.enums.PaymentMethod;
+import payment.enums.PaymentResult;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "order_payments")
+@Entity
 public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long orderId;
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PaymentMethod paymentMethod = PaymentMethod.NONE;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PaymentResult paymentResult = PaymentResult.PENDING_PAYMENT;
+
 }
